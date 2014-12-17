34ee90eb246e8e8762513467e52681ad5af63395
philipjrosen <philipjrosen@gmail.com>
2014-12-16 19:56:40 -0800

    deletes stray copy of madonnaDancer

--- madonnaDancer.js
+++
@@ -1,8 +0,0 @@
-var MadonnaDancer = function(top, left, timeBetweenSteps) {
-  this.$node = $('<div class="madonna"></div>');
-  Dancer.call(this);
-  console.log(this);
-};
-
-MadonnaDancer.prototype = Object.create(Dancer.prototype);
-MadonnaDancer.prototype.constructor = MadonnaDancer;
